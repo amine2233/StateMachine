d99b1d8f1faab03c69071af43f9c34338ad1e9a7
Amine Bensalah <amine.bensalah@intech-consuting.fr>
2018-09-30 19:02:22 +0200

    first commit

---
+++ Sources/StateMachine.swift
@@ -0,0 +1,8 @@
+//
+//  StateMachine.swift
+//  StateMachine
+//
+//  Created by Amine Bensalah on 30/09/2018.
+//
+
+import Foundation
---
+++ Sources/StateMachine_iOS.h
@@ -0,0 +1,18 @@
+//
+//  StateMachine_iOS.h
+//  StateMachine iOS
+//
+//  Created by Amine Bensalah on 30/09/2018.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StateMachine_iOS.
+FOUNDATION_EXPORT double StateMachine_iOSVersionNumber;
+
+//! Project version string for StateMachine_iOS.
+FOUNDATION_EXPORT const unsigned char StateMachine_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StateMachine_iOS/PublicHeader.h>
+
+
